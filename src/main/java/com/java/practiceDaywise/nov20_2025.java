3616a4ffc429be8450b640bf38dda93e1c3a6654
pn97arjun <pn97arjun@gmail.com>
2025-11-20 21:34:26 +0530

    nov 18

--- src/main/java/com/java/practiceDaywise/nov20_2025.java
+++ src/main/java/com/java/practiceDaywise/nov20_2025.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class nov20_2025 {
 
@@ -159,6 +161,105 @@ public class nov20_2025 {
         }
         for(int i=0;i<c.length;i++)
             System.out.print(c[i]+" ");
+    }
+    @Test
+    public void duplicateoccurencenov20()
+    {
+        String str="selenium l";
+        char[] c=str.toCharArray();
+        for(int i=0;i<c.length;i++)
+        {
+            int count=1;
+            for (int j=i+1;j<c.length;j++)
+            {
+                if(c[i]==c[j] && c[j]!=' ')
+                {
+                    count++;
+                    c[j]='0';
+                }
+            }
+            if(count>1 && c[i]!='0')
+                System.out.println(count+" "+c[i]);
+        }
+    }
+    @Test
+    public void reverseEachWord()
+    {
+        String input="Test Automation";
+        String[] words=input.split(" ");
+        StringBuilder reverseSentence=new StringBuilder();
+        for(String word:words)
+        {
+            StringBuilder reverseWord=new StringBuilder(word);
+            reverseSentence.append(reverseWord.reverse()).append(" ");
+
+        }
+        System.out.println(reverseSentence.toString().trim());
+    }
+    @Test
+    public void rpayramidnov20()
+    {
+        int rows=5;
+        for(int i=1;i<=rows;i++)
+        {
+            for(int j=i;j<rows;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=1;k<=(2*i)-1;k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    @Test
+    public void secondSmallest()
+    {
+        int[] arr=new int[]{4,2,3,1,5};
+        int smallest=Integer.MAX_VALUE;
+        int secondSmaleest=Integer.MAX_VALUE;
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i]<smallest)
+            {
+                secondSmaleest=smallest;
+                smallest=arr[i];
+            }
+            if(arr[i]<secondSmaleest && arr[i]!=smallest)
+            {
+                secondSmaleest=arr[i];
+            }
+        }
+        System.out.println(secondSmaleest);
+    }
+    @Test
+    public void factornov20()
+    {
+        int factorial=1;
+        int n=5;
+        for(int i=1;i<=n;i++)
+        {
+            factorial*=i;
+        }
+        System.out.println(factorial);
+    }
+    @Test
+    public void duplicateWords()
+    {
+        String[] c = {"zim", "aus", "bang", "cat", "zim"};
+
+        Set<String> seenWords = new HashSet<>();
+        Set<String> duplicateWords = new HashSet<>();
+
+        for (String word : c) {
+            // If the word has been seen before, it's a duplicate.
+            if (!seenWords.add(word)) {
+                duplicateWords.add(word);
+            }
+        }
+
+        System.out.println("The duplicate word(s) is/are: " + duplicateWords);
     }
 
 }
